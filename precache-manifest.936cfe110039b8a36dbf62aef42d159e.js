fd1bf126f99c08e98e30069ca69408bb0905252f
juhengze <juhengze_v@didiglobal.com>
2020-02-22 01:25:13 +0800

    mineweeper

---
+++ precache-manifest.936cfe110039b8a36dbf62aef42d159e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38b4b700fa3fe8284da38555ef4d56b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f92a96ca42eeb046f7",
+    "url": "/static/css/index.144ae105.chunk.css"
+  },
+  {
+    "revision": "b4f92a96ca42eeb046f7",
+    "url": "/static/js/index.80ea37.chunk.js"
+  },
+  {
+    "revision": "cc2961815c0b040d7e83",
+    "url": "/static/js/runtime~index.a8a9905a.js"
+  },
+  {
+    "revision": "558cdb3d23db0be019e6",
+    "url": "/static/js/vendors~index.791531.chunk.js"
+  }
+]);
